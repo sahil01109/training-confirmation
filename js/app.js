14bb1f8d999425340388d1c7c3a80d10590c991f
sahil01109 <sahilk25876@gmail.com>
2026-07-23 09:49:10 -0700

    print remove

--- js/app.js
+++ js/app.js
@@ -274,20 +274,3 @@ document
 );
 
 
-// =========================================
-// Print
-// =========================================
-
-document
-.getElementById("printBtn")
-.addEventListener(
-
-    "click",
-
-    function(){
-
-        window.print();
-
-    }
-
-);
